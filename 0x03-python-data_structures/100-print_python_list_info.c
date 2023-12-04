2d23d2e82c6e5137990913feb835a55ebe200f2b
Erickirea <ericfoxe98@gmail.com>
2023-12-04 13:02:15 -0800

    corrections

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -3,7 +3,7 @@
 #include <listobject.h>
 
 /**
-* print_python_list_info - prints some basic info about pytohn list
+* print_python_list_info - prints some basic info about python list
 * @p: python object
 **/
 
@@ -18,4 +18,3 @@ void print_python_list_info(PyObject *p)
 	for (i = 0; i < size; i++)
 		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
 }
-
