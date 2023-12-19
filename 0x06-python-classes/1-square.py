a81421132cd0aed76de4833b62ab5a501c8691cf
Erickirea <ericfoxe98@gmail.com>
2023-12-19 14:34:11 -0800

    initial commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""a class Square that defines a square by: (based on 0-square.py) """
+class Square:
+    """Represents a square"""
+    def __init__(self, size):
+        """Initializing this square class
+        Args: size - represents the size of the square defined
+        """
+
+        self.__size = size
